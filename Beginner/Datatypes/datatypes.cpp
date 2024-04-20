698128213eb93fc951b76947364e6f06c4eaa1d6
aarambhpatil <mortalfear8611@gmail.com>
2024-04-21 05:19:09 +0530

    New Commit

---
+++ Beginner/Datatypes/datatypes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Size of int: "<<sizeof(a)<<endl;
+    
+    float b;
+    cout<<"Size of float: "<<sizeof(b)<<endl;
+    
+    char c;
+    cout<<"Size of char: "<<sizeof(c)<<endl;
+    
+    bool d;
+    cout<<"Size of bool: "<<sizeof(d)<<endl;
+    
+    short int e;
+    cout<<"Size of short int: "<<sizeof(e)<<endl;
+    
+    long int f;
+    cout<<"Size of long int: "<<sizeof(f)<<endl;
+    
+    long long int g;
+    cout<<"Size of long long int: "<<sizeof(g)<<endl;
+}
